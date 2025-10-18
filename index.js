6ba11a03299525721c41a27b3260c9dfefd3663e
kittu011020 <kittu011020@gmail.com>
2025-10-18 13:34:05 +0530

    Update and rename Main.py to index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ try {
 }
 
 // ✅ Group Info
-const GROUP_THREAD_ID = "24196335160017473";
-const LOCKED_GROUP_NAME = "🤪 EXIT FUNNY KIDX + TUSHAR BOKA CHUDKE DAFAN 😂";
+const GROUP_THREAD_ID = "25129115266701148";
+const LOCKED_GROUP_NAME = "🤪 EXIT FUNNY KIDX + Abhi jaat ki duniya";
 
 // ✅ Express Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
